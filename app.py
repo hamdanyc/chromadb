c7bd87c81c24089543709d38d011ffc558d142a9
Hamdan Yaccob Kol (Rtd) <113873149+hamdanyc@users.noreply.github.com>
2025-09-02 21:35:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -54,6 +54,7 @@ collections = client.list_collections()
 pdf = [collection.name for collection in collections]
 qs = ["Summarize the text",
       "Summarize the text. Identify main theme of the text and the key findings and list theme and finding as a JSON format",
+      "What is the title of the text",
       "Give the abstract from the article",
       "What data analysis mentioned in the text",
       "What has been studied on this topic?",
